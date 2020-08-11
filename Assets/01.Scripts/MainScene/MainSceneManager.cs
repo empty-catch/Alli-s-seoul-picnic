cd376c50224d095a2613ab661942ee420b0f91f7
Komorio <twilivez@gmail.com>
2020-08-12 02:02:32 +0900

    [ADD] MainScene 추가

---
+++ Assets/01.Scripts/MainScene/MainSceneManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class MainSceneManager : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private CanvasGroup titleGroup;
+
+    [SerializeField]
+    private CanvasGroup tutorialGroup;
+
+    [SerializeField]
+    private CanvasGroup stageGroup;
+
+    private bool exitTitle;
+    private bool tutorialing;
+    
+    private void Start(){
+        if(PlayerPrefs.GetInt("Retry",0) == 1){
+            OpenStageGroup();
+        } else {
+            OpenTitleGroup();
+        }
+    }
+
+    public void OpenTitleGroup(){
+        titleGroup.gameObject.SetActive(true);
+        if(exitTitle){
+            Tween openTween = stageGroup.DOFade(0.0f, 0.25f);
+            
+            openTween.OnComplete(() => {
+                stageGroup.gameObject.SetActive(false);
+                titleGroup.DOFade(1.0f, 0.25f);
+            });
+        } else {
+            exitTitle = true;
+            Tween openTween = titleGroup.DOFade(1.0f, 0.25f);
+        }
+    }
+
+    public void OpenTutorial(){
+        Tween openTween = titleGroup.DOFade(0.0f, 0.25f);
+        openTween.OnComplete(() => {
+            titleGroup.gameObject.SetActive(false);
+            tutorialGroup.gameObject.SetActive(true);
+            tutorialGroup.DOFade(1.0f, 0.25f);
+
+            tutorialing = true;
+        });
+    }
+
+    public void OpenStageGroup(){
+        Tween openTween = tutorialGroup.DOFade(0.0f, 0.25f);
+        openTween.OnComplete(() => {
+            tutorialing = false;
+
+            tutorialGroup.gameObject.SetActive(false);
+            stageGroup.gameObject.SetActive(true);
+            stageGroup.DOFade(1.0f, 0.25f);
+        });
+    }   
+
+    public void GameStart(){
+        SceneManager.LoadScene("Game");
+    }
+
+    private void Update(){
+        if(tutorialing){
+            if(Input.anyKeyDown){
+                OpenStageGroup();
+            }
+        }
+    }
+}
---
+++ Assets/01.Scripts/MainScene/StageButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class StageButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lightObject;
+    public static List<StageButton> stageButtons = new List<StageButton>();
+    private Vector3 defaultScale;
+    private Vector3 downScale;
+    private Vector3 upScale;
+
+    public static StageButton selectButton;
+    
+    private void Awake(){
+        StageButton.stageButtons.Add(this);
+    }
+
+    private void Start(){
+        defaultScale = gameObject.transform.localScale;
+        downScale = defaultScale * 0.9f;
+        upScale = defaultScale * 1.1f;
+    }
+
+    public static void AllDown(){
+        stageButtons.ForEach((item) => {
+            if(item != StageButton.selectButton){
+                item.DownScale();
+            }
+        });
+    } 
+    
+    public void ChangeScale(){
+        lightObject.SetActive(true);
+        gameObject.transform.DOScale(upScale, 0.5f);
+    }
+
+    public void DownScale(){
+        lightObject.SetActive(false);
+        gameObject.transform.DOScale(downScale, 0.5f);
+    }
+
+    public void SetButton(int index){
+        PlayerPrefs.SetInt("Difficulty", index);
+        StageButton.selectButton = this;
+        StageButton.AllDown();
+
+        ChangeScale();
+    }
+
+    public static void Reset(){
+        StageButton.stageButtons.ForEach((item) => {
+            item.DownScale();
+        });
+    }
+}
